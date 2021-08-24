9f789243e75eee05023fbd3506dafe0bae3bd00d
lvanhee <lois.vanhee@gmail.com>
2021-08-24 19:26:04 +0200

    Minor update

--- src/markov/cvar/CvarDemo.java
+++ src/markov/cvar/CvarDemo.java
@@ -10,7 +10,7 @@ import markov.probas.DiscreteProbabilityDistributionAccuracyParameters;
 public class CvarDemo {
 
 	public final static DiscreteProbabilityDistributionAccuracyParameters CVAR_PARAMS = new DiscreteProbabilityDistributionAccuracyParameters(200,0.0001);
-	public static final double DEFAULT_CVAR_DISCOUNT = 0.95;
+	public static final double DEFAULT_CVAR_DISCOUNT = 0.7;
 	
 	public static void main(String[] args)
 	{
